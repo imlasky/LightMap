a11b1e3bfff0704502dc42ea7e65f1eeca03bf9f
zac <zac.freer@knights.ucf.edu>
2017-06-08 17:43:46 -0400

    Main script that imports the rest of the modules
    Currently loads GUI and ImageProcessing modules

---
+++ LightMap.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 17:19:13 2017
+
+@author: dellwick
+"""
+
+import ImageProcessing as ip
+import gui
+
+if __name__ == "__main__":
+    
+    g = gui.GUI()
+    
+    img_pro = ip.ImageProcessing(g.fileChosen)
+    
+    image = img_pro.file_sorting()
+    
