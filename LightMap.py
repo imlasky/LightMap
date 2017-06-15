18140e021f7b4033476045605aabc60d27114646
zac <zac.freer@knights.ucf.edu>
2017-06-15 15:00:47 -0400

    Main class to launch img processing and projecting

--- LightMap.py
+++ LightMap.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jun  8 17:19:13 2017
-
-@author: dellwick
-"""
 
 import ImageProcessing as ip
-import gui_Tkinter as gui
-
-if __name__ == "__main__":
+import cv2
     
-    g = gui.GUI()
+class LightMap():
+    def __init__(self, filename):
+        img_pro = ip.ImageProcessing(filename)
+        image = img_pro.file_sorting()
     
-    img_pro = ip.ImageProcessing(g.fileChosen)
-    
-    image = img_pro.file_sorting()
-    
+        for i in range(len(image)):
+            cv2.imwrite("output{}.png".format(i), image[i])
