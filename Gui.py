35652b0feebc924bc679008851367ed6a88c7bd5
FridaySky <kevintran1196@Knights.ucf.edu>
2017-06-16 21:29:29 -0400

    File Path Correctly Displays File
    ~Kevin

--- Gui.py
+++ Gui.py
@@ -4,12 +4,12 @@
     Graphical User Interface for LightMap
 '''
 
+#Standard imports.
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtCore import pyqtSlot
 from PyQt5 import uic
-#import LightMap
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import *
 
 #Leverage an object-oriented approach to create the GUI.
 class GUI(QMainWindow):
@@ -17,32 +17,34 @@ class GUI(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        #Set the window icon.
+        #Define the window icon.
         self.window_icon = QIcon("LightMap.png")
 
+        #Make the UI.
         self.init_ui()
     
     #Fill the GUI.
     def init_ui(self):
-        #Set the window icon.
-        self.setWindowIcon(self.window_icon)
-
         #Load the UI file.
         main_window = uic.loadUi("mainwindow.ui", self)
+
+        #Set the window icon.
+        self.setWindowIcon(self.window_icon)
 
         #Add commands for actions under the submenus.
         self.command_file_menu(main_window)
         self.command_settings_menu(main_window)
         self.command_help_menu(main_window)
 
-        #Implement back-end logic for the Open Image button.
-        self.file_name = "No File Selected"
-        main_window.label_file_name.setText(self.file_name)
+        #Handle the case that the user clicks on the "Open Image" button.
         main_window.button_open_image.setStatusTip("Open Image")
-        main_window.button_open_image.clicked.connect(self.open_file)
+        main_window.button_open_image.clicked.connect(lambda: self.open_file(main_window))
 
-        #Start mapping.
-        main_window.button_start_mapping.setStatusTip("Start Mapping!")
+        #Make sure this variable has been declared so that we can click "Start Mapping" at any time.
+        self.file_chosen = None
+
+        #Handle the case that the user clicks on the "Start Mapping" button.
+        main_window.button_start_mapping.setStatusTip("Start Mapping")
         main_window.button_start_mapping.clicked.connect(self.start_mapping)
 
         #Show the main window.
@@ -51,8 +53,7 @@ class GUI(QMainWindow):
     def start_mapping(self):
         #Check if the user has selected an image before sending the file path to the main program.
         if not self.file_chosen:
-            pass
-            #messagebox.showinfo("No File Selected", "Please select a valid image file before mapping!")
+            QMessageBox.about(self, "No File Selected", "Please select a valid image file before mapping!")
         else:
             return self.file_chosen
 
@@ -61,7 +62,7 @@ class GUI(QMainWindow):
         #Back-end logic for Open Image.
         main_window.action_open_image.setShortcut("CTRL+O")
         main_window.action_open_image.setStatusTip("Open Image")
-        main_window.action_open_image.triggered.connect(self.open_file)
+        main_window.action_open_image.triggered.connect(lambda: self.open_file(main_window))
 
         #Back-end logic for Record Video.
         main_window.action_record_video.setShortcut("CTRL+R")
@@ -88,27 +89,18 @@ class GUI(QMainWindow):
         main_window.action_About_LightMap.setStatusTip("About LightMap")
 
     #Open an image file.
-    def open_file(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
+    def open_file(self, main_window):
+        #Open the file dialog to select an image file.
         self.file_chosen, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
-            "JPEG (*.JPEG *.jpeg *.JPG *.jpg *.JPE *.jpe *JFIF *.jfif);; PNG (*.PNG *.png);; GIF (*.GIF *.gif);; Bitmap Files (*.BMP *.bmp *.DIB *.dib);; TIFF (*.TIF *.tif *.TIFF *.tiff);; ICO (*.ICO *.ico)", options=options)
+            "JPEG (*.JPEG *.jpeg *.JPG *.jpg *.JPE *.jpe *JFIF *.jfif);; PNG (*.PNG *.png);; GIF (*.GIF *.gif);; Bitmap Files (*.BMP *.bmp *.DIB *.dib);; TIFF (*.TIF *.tif *.TIFF *.tiff);; ICO (*.ICO *.ico)")
         
         #Show the path of the file chosen.
         if self.file_chosen:
-            self.file_name = self.file_chosen
+            main_window.label_file_name.setText(self.file_chosen)
         else:
-            self.file_name = "No image was selected. Please select an image."
-        print(self.file_chosen)
+            main_window.label_file_name.setText("No image was selected. Please select an image.")
 
-    #Return the image file name to the back end.
-    def return_file(self):
-        #Check if the user has selected an image before sending the file path to the main program.
-        if not self.file_chosen:
-            QMessagebox.showinfo(self, "No File Selected", "Please select a valid image file before mapping!")
-
-
-
+#DEBUG: DELETE LATER
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = GUI()
