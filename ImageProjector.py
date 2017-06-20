c70e34e35e7ef3110225b5902d46abf1fd868855
AlecRomine <rominealec@gmail.com>
2017-06-20 14:31:26 -0400

    Add files via upload

---
+++ ImageProjector.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+'''
+    Class: COP4331C (Summer 2017)
+    Group: G13
+    projectImage for LightMap
+'''
+
+import pygame
+import sys
+
+class ImageProjector:
+    
+    def __init__(self,screen):
+        self.screen = screen
+    
+    def project_image(self,image,(x,y)):   
+    
+        #fill background with in black
+        self.screen.fill((0,0,0))
+    
+        #place image at new location
+        self.screen.blit(image,(int(x),int(y)))
+    
+        #Updates the display
+        pygame.display.flip()
+    
+        return
