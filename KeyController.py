48f928fb40b0d0f92f7d6e830c1876e159af1eab
imlasky <ian.lasky1@gmail.com>
2017-06-28 12:30:14 -0400

    changed to work with calibration

--- KeyController.py
+++ KeyController.py
@@ -16,15 +16,11 @@ class KeyController:
     
     def __init__(self,screen):
         self.screen = screen
-
      
     def check_keys(self):
         self.events = pygame.event.get()
         self.exit_flag = False
-        self.up_flag = False
-        self.down_flag = False
-        self.right_flag = False
-        self.left_flag = False
+        self.enter_flag = False
         for event in self.events:
             if event.type == KEYUP:
                 if event.key == pygame.K_q:
@@ -33,17 +29,11 @@ class KeyController:
                     self.exit_flag = True
                 elif event.key == pygame.K_ESCAPE:
                     self.exit_flag = True
-                elif event.key == pygame.K_UP:
-                    self.up_flag = True
-                elif event.key == pygame.K_DOWN:
-                    self.down_flag = True
-                elif event.key == pygame.K_RIGHT:
-                    self.right_flag = True
-                elif event.key == pygame.K_LEFT:
-                    self.left_flag = True
+                elif event.key == pygame.K_RETURN:
+                    self.enter_flag = True
             elif event.type == QUIT:
                 self.exit_flag = True
 
-        return (self.exit_flag, self.up_flag, self.down_flag, self.right_flag, self.left_flag)
+        return (self.exit_flag, self.enter_flag)
             
             
