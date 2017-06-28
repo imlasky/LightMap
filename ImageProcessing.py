077bf60ac964273ca6c115aed4fad3f460e928ed
zac <zac.freer@knights.ucf.edu>
2017-06-27 23:35:33 -0400

    Performance optimization with handling gifs

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -13,21 +13,15 @@ from PIL import Image
 from math import floor
 from os import remove
 
-
-# Differentiates from animated and nonanimated image formats and crops them 
-# into a circle with an alpha layer in the background.
 class ImageProcessing():
     def __init__(self):
-        self.size = ()
-        
-        
-        
-    # Sorts between animated and nonanimated images
-    def file_sorting(self, filename):
+        self.filename = ""
+        np.set_printoptions(threshold=np.inf)
+    
+    def image_processing(self, filename):
         self.filename = filename
         
         animated_formats = ['gif']
-        #, 'apng', 'mng', 'svg', 'svgz', 'webp']
         
         info = magic.from_file(self.filename, mime=True)
         info = info.split('/')
@@ -36,101 +30,104 @@ class ImageProcessing():
             raise TypeError("Application doesn't support this file format")
         
         elif info[1] not in animated_formats:
-            return self.__unanimated_image()
+            cropped_images = self.unanimated_image()
             
         else:
-            return self.__animated_image()
-    
-    # Handles all non animated file formats        
-    def __unanimated_image(self):
+            cropped_images = self.animated_image()
+        
+        return cropped_images
+           
+    def unanimated_image(self):
         image = []
         
         image.append(cv2.imread(self.filename, 1))
         
-        image[0] = self.__generate_circle(image[0])
-        
+        image = self.generate_circle(image)
+    
         return image
             
     # Creates circular image with a transparent background
-    def __generate_circle(self, image):
-        image = self.__crop_circle(image)
-        image = self.__add_alpha(image)
+    def generate_circle(self, image, frame_number):
+
+        image = self.crop_circle(image, frame_number)
         
-        #cv2.imwrite("output.png", image)
+        image = self.add_alpha(image, frame_number)
+           
         return image
-        
-    
+
     # outlines the largest, center circle in the image  
-    def __crop_circle(self, image):
-        self.__get_size(image)
-        index = self.__find_min(self.size[0], self.size[1])
+    def crop_circle(self, image, frame_number):
+        dimensions = self.get_size(image)
+        index = self.find_min(dimensions[0], dimensions[1])
         
-        radius = floor(self.size[index] / 2)
-        center = (floor(self.size[0] / 2), floor(self.size[1] / 2))
+        radius = floor(dimensions[index] / 2)
+        center = (floor(dimensions[0] / 2), floor(dimensions[1] / 2))
         
-        
-        circle = np.zeros(self.size, np.uint8)
+        circle = np.zeros(dimensions, np.uint8)
         
         cv2.circle(circle, (center[1], center[0]), radius, 1, thickness=-1)
         
         image = cv2.bitwise_and(image, image, mask=circle)
         
-        image = self.__trim_square(image, center, radius)
+        image = self.trim_square(image, center, radius, frame_number)
         
         return image
         
     # Crops the square surrounding the largest, centermost circle of the image 
-    def __trim_square(self, image, center, radius):
+    def trim_square(self, image, center, radius, frame_number):
         image = image[(center[0] - radius):(center[0] + radius),
                       (center[1] - radius):(center[1] + radius)]
-    
+          
         return image
         
         
     # Adds alpha layer behind circle image
-    def __add_alpha(self, image):
+    def add_alpha(self, image, frame_number):
         greyscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         _, alpha = cv2.threshold(greyscale, 0, 255, cv2.THRESH_BINARY)
         b, g, r = cv2.split(image)
         
-        fourlayer = [b, g, r, alpha]
-        return cv2.merge(fourlayer, 4)
+        fourlayers = [b, g, r, alpha]
+        
+        image = cv2.merge(fourlayers, 4)
+        
+        return image
         
         
     # Returns the number of the paramater of the shorter dimension
-    def __find_min(self, x, y):
+    def find_min(self, x, y):
         if x < y:
             return 0
         else:
             return 1        
             
     # Gets the dimensions of the input image
-    def __get_size(self, image):
-        self.size = image.shape[:2]
-        
-        
-    # Handles animated image formats 
-    def __animated_image(self):
-        #raise TypeError("Application doesn't support this file format")
-        
+    def get_size(self, image):
+        size = image.shape[:2]
+        return size
+ 
+    def animated_image(self):
         frames = []
+        
         gif = Image.open(self.filename)
         
-        for i, frame in enumerate(self.__gif_divorce(gif)):
-            frame.save('temp.png', **frame.info)
-            frames.append(cv2.imread('temp.png', 1))
-            remove('temp.png')
-            
-            # list of png frames created
-            
-        for i in range(len(frames)):
-            frames[i] = self.__generate_circle(frames[i])
-            #cv2.imwrite("output%d.png" % i, frames[i])
+        gif_frames = self.gif_divorce(gif)
+
+        for i in range(len(gif_frames)):    
+            image = self.generate_circle(gif_frames[i], i)
+            frames.append(image)
             
         return frames
-            
-    # Breaks gif into frames and stores into list
-    def __gif_divorce(self, gif):
+    
+    def get_frames(self, frame_info):
+        frame_info.save('temp.png', **frame_info.info)
+        frame = cv2.imread('temp.png', 1)
+        remove('temp.png')
+        
+        return frame
+    
+    def gif_divorce(self, gif):
+        frames = []
         try:
             i = 0
             while True:
@@ -143,10 +140,11 @@ class ImageProcessing():
                 else:
                     imgframe.putpalette(palette)
                     
-                yield imgframe
+                current_frame = self.get_frames(imgframe)
                 
+                frames.append(current_frame)
+        
                 i += 1
                 
         except EOFError:
-            pass
-                
+            return frames
