a037bd6f0e418769799104e12b6fdbee179aaed0
imlasky <ian.lasky1@gmail.com>
2017-07-20 10:48:26 -0400

    changed calibration

--- LightMap/CalibrateScreen.py
+++ LightMap/CalibrateScreen.py
@@ -11,6 +11,7 @@ import numpy as np
 import pygame
 import KeyController as kc
 import Detector
+import time
 from scipy.interpolate import interp1d
 
 class Calibrate:
@@ -20,12 +21,11 @@ class Calibrate:
         self.x_offset = 0
         self.y_offset = 0
         self.coords_ind = 0
-        self.x_offsets = []
-        self.y_offsets = []
+        self.x_locs = []
+        self.y_locs = []
         self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
         self.myKc = kc.KeyController(self.screen)
         self.detect = Detector.Detector()
-        
         self.__calibrate()
         
         
@@ -38,58 +38,51 @@ class Calibrate:
         
         w, h = self.screen.get_size()
         
-        self.coords = np.array([[int(w/2),int(h/2)],[int(w/4),int(h/2)],[int(w/2),int(3*h/4)],
-                   [int(3*w/4),int(h/2)]])
+    
+        self.screen_width_space = np.linspace(50,w-50,50,dtype=np.uint16)
+        self.screen_height_space = np.linspace(50,h-50,50,dtype=np.uint16)
+        
         
         
         
 
 
+        i = 0
         while True:
             
-            #start with a black screen
             self.screen.fill((0,0,0))
             
-            x_loc, y_loc, radius = self.detect.readFramesHough()
-            im = pygame.image.load('../Images/Calibration.png')
-            im = pygame.transform.scale(im,(50,50))
-            im_rect = im.get_rect()
-            im_rect.centerx = int(w/2)
-            im_rect.centery = int(h/2)
-            
-            if im_rect:
-                self.screen.blit(im,((self.coords[self.coords_ind,0]-int(im_rect.width/2)),self.coords[self.coords_ind,1]-int(im_rect.height/2)))
-                if radius > 0:
-                    pygame.draw.circle(self.screen,(255,255,0),(self.coords[self.coords_ind,0],self.coords[self.coords_ind,1]),radius,1)
-            
+            pygame.draw.circle(self.screen,(255,0,0),(self.screen_width_space[i],self.screen_height_space[i]),50,5)
             pygame.display.flip()
+            x_loc, y_loc, radius = self.detect.readFramesHough()
+            
+            
+            self.x_locs.append(x_loc)
+            self.y_locs.append(y_loc)
             flags = self.myKc.check_keys()
-            if flags[1] and radius > 0:
-                if self.coords_ind == 3:
-                    self.x_offsets.append(self.coords[self.coords_ind,0] - x_loc)
-                    self.y_offsets.append(self.coords[self.coords_ind,1] - y_loc)
-                    break
-                self.x_offsets.append(self.coords[self.coords_ind,0] - x_loc)
-                self.y_offsets.append(self.coords[self.coords_ind,1] - y_loc)
-                self.coords_ind += 1
-            if flags[0]:
+
+            i += 1    
+
+            if flags[0] or i >= len(self.screen_width_space):
                 self.detect.stopRead()
                 pygame.display.quit()
                 pygame.quit()
-                return
-
-        
+                break
+            
+            time.sleep(0.2)
+            
         self.detect.stopRead()
         self.__interp()
         pygame.display.quit()
         pygame.quit()
+
         
     def __interp(self):
         
-        self.fx = interp1d(self.coords[:,0],self.x_offsets,bounds_error=False,
+        self.fx = interp1d(self.x_locs,self.screen_width_space,bounds_error=False,
                            fill_value=0)
         
-        self.fy = interp1d(self.coords[:,1],self.y_offsets,bounds_error=False,
+        self.fy = interp1d(self.y_locs,self.screen_height_space,bounds_error=False,
                            fill_value=0)
         
     def getOffsets(self,xold,yold):
